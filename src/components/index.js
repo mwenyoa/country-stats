5e01a625701ff796840b72fe42cb25423d2c14a9
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-08-01 16:52:22 +0200

    Export all components from its index file

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Header from './header';
+import Footer from './footer';
+import BackToTopButton from './backOnTop';
+import Loading from './Loader';
+
+export {
+  Header, Footer, BackToTopButton, Loading,
+};
