2c59e2dabcd4eb5cfa530ba7eb7b4aa83e23cd28
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-08-01 16:53:35 +0200

    Refactor router defination

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-import React from "react";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import { BrowserRouter as Router } from "react-router-dom";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom/client';
+import store from './redux/store';
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
