8f4cafaf81a28a176d0e076913556f4e846cd948
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-08-01 16:53:48 +0200

    Render application

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react'; 
-import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NotFound from './notfound';
-import { Countries, Country } from './pages';
+import {
+  Header, Footer, BackToTopButton, Loading,
+} from './components';
 import './App.css';
 
-
 function App() {
+  const ListCountries = lazy(() => import('./pages/countries'));
+  const ShowCountry = lazy(() => import('./pages/country'));
   return (
     <div className="App">
       {/* App Routes */}
-      <Routes>
-         <Route path='/' element={<Countries />} />
-         <Route path='countries/:id' element={<Country />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Router>
+        <Header />
+        <main className="min-h-screen flex flex-col items-center">
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/" element={<ListCountries />} />
+              <Route path="countries/:id" element={<ShowCountry />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+          <BackToTopButton />
+        </main>
+        <Footer />
+      </Router>
     </div>
   );
 }
