c53535bdccfeafdb2229460424598abbcbb7b4fb
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-08-01 16:47:33 +0200

    Fix errors

--- src/hooks/useFetchCountry.js
+++ src/hooks/useFetchCountry.js
@@ -1,16 +1,13 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchCountry } from "../redux/services/api-service";
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCountry } from '../redux/services/api-service';
 
-const  useFetchCountry = (countryId) => {
+const useFetchCountry = (countryId) => {
   const dispatch = useDispatch();
   const { country } = useSelector((state) => state.country);
-
   useEffect(() => {
-    if (country === undefined) {
-      dispatch(fetchCountry(countryId));
-    }
-  }, [country, dispatch]);
+    dispatch(fetchCountry(countryId));
+  }, [countryId, dispatch]);
 
   return country;
 };
