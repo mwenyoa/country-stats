1b6f72773d9a654f7fcdc192212410a9a9560d1f
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-08-01 16:41:25 +0200

    Create country reducer to handle singlre country state slice

--- src/redux/reducers/country-reducer.js
+++ src/redux/reducers/country-reducer.js
@@ -1,30 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { fetchCountry } from "../services/api-service";
+/* eslint-disable no-param-reassign */
+/* eslint-disable prefer-destructuring */
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchCountry } from '../services/api-service';
 
 const initialState = {
   country: {},
-  isLoaded: false,
+  isLoaded: 'idle',
   error: undefined,
 };
 
 const countrySlice = createSlice({
-  name: "country",
+  name: 'country',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchCountry.pending, (state) => {
-        state.isLoaded = false;
+        state.isLoaded = 'pending';
       })
       .addCase(fetchCountry.fulfilled, (state, action) => {
-        state.country = action.payload;
-        state.isLoaded = true;
+        state.country = action.payload[0];
+        state.isLoaded = 'loaded';
       })
       .addCase(fetchCountry.rejected, (state, action) => {
         state.error = action.error.message;
-        state.isLoaded = true;
+        state.isLoaded = 'loaded';
       });
   },
 });
 
 export default countrySlice.reducer;
+/* eslint-disable no-param-reassign */
+/* eslint-disable prefer-destructuring */
