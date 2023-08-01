83e730f726be0d99242d827fc94c01dfc7296794
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-08-01 16:45:43 +0200

    Create  country's details page

--- src/pages/country.js
+++ src/pages/country.js
@@ -1,16 +1,99 @@
-import { useParams } from "react-router-dom";
-import useFetchCountry from "../hooks/useFetchCountry";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import useFetchCountry from '../hooks/useFetchCountry';
+import 'aos/dist/aos.css';
 
-const Country = ()  => {
-    const param = useParams();
-    const countryId = param.id;
-    const country = useFetchCountry(countryId);
- 
-         <section className="max-w-[75%] mx-auto shadow-2xl">
-            <img src=""></img>
-            {countryId }
-         </section>
-    )
-}
+const Country = () => {
+  const param = useParams();
+  const country = useFetchCountry(param.id);
+  const {
+    flags,
+    name,
+    population,
+    timezones,
+    area,
+    unMember,
+    borders,
+    capital,
+    landlocked,
+    latlng,
+    region,
+    subregion,
+    startOfWeek,
+  } = country;
+
+  return (
+    <section
+      data-aos="zoom-in"
+      className="relative country-card flex flex-col items-center mx-auto h-full md:max-w-[80%] lg:max-w-[75%] xl:max-w[75%] rounded-xl country-details"
+    >
+      <div className="relative md:flex py-20 h-full md:justify-between  md:max-w-[85%] lg:max-w-[80%] xl:max-w-[75%] mx-auto  px-2">
+        <img
+          src={flags?.svg}
+          alt={flags?.alt}
+          className="details-img mx-auto xs:max-w-[95%] sm:max-w-[90%] sm:mx-auto md:max-w-[50%] lg:max-w-[50%] xl:max-w-[50%]  px-5"
+        />
+        <div className="country-details flex w-full xs:max-w-[95%] sm:max-w-[90%] sm:mx-auto md:max-w-[100%] lg:max-w-[100%] xl:max-w-[100%]  mx-auto  flex-col justify-between px-5 leading-9">
+          <h1 className="flex flex-wrap text-center xs:text-2xl sm:text-2xl md:text-4xl lg:text-4xl xl:text-4xl">
+            {name?.official}
+          </h1>
+          <h2 className="flex justify-between">
+            <span>Capital: </span>
+            <span>{capital}</span>
+          </h2>
+          <h2 className="flex justify-between">
+            <span>population: </span>
+            {' '}
+            {population}
+          </h2>
+          <h2 className="flex justify-between">
+            <span>Region: </span>
+            {' '}
+            {region}
+          </h2>
+          <h2 className="flex justify-between">
+            <span>Sub Region: </span>
+            {' '}
+            {subregion}
+          </h2>
+          <h2 className="flex justify-between">
+            <span>Area: </span>
+            {' '}
+            {area}
+          </h2>
+          <h2 className="flex justify-between">
+            <span>UN Member </span>
+            {' '}
+            {unMember ? 'Yes' : 'No'}
+          </h2>
+          <h2 className="flex justify-between">
+            <span>Land Locked: </span>
+            {' '}
+            {landlocked ? 'Yes' : 'No'}
+          </h2>
+          <h2 className="flex justify-between">
+            <span>Co-ordinates: </span>
+            {' '}
+            {latlng?.join(' , ')}
+          </h2>
+          <h2 className="flex justify-between">
+            <span>Time Zone: </span>
+            {' '}
+            {timezones?.[0]}
+          </h2>
+          <h2 className="flex justify-between">
+            <span>Borders: </span>
+            {' '}
+            <span className="flex flex-flow px-2">{borders?.join(' , ')}</span>
+          </h2>
+          <h2 className="flex justify-between">
+            <span>Week Starts On: </span>
+            <span>{startOfWeek}</span>
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Country;
