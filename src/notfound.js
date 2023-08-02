9b727d07bc0d74ea4c36d60b740a6fec24762911
Anthony Mwenyo <mwenyoanthony@gmail.com>
2023-08-02 15:40:43 +0200

    refactor  styling defination on notfound page

--- src/notfound.js
+++ src/notfound.js
@@ -5,9 +5,9 @@ const NotFound = () => (
     <div className="container">
       <div className="row">
         <div className="col-sm-8 offset-sm-2 text-gray-50 text-center -mt-52">
-          <div className="relative flex flex-col justify-center items-center mt-40 px-5">
+          <div className="relative xs:text-4xl sm:text-6xl md:text:8xl flex flex-col justify-center items-center mt-40 px-5">
             <h1 className="relative text-8xl tracking-tighter-less text-shadow font-sans font-bold">
-              <span className="relative text-4xl top-0 -ml-24  -mt-24 text-gray-300 font-semibold">
+              <span className="relative text-4xl top-0 -ml-12  -mt-24 text-gray-300 font-semibold">
                 Oops!
               </span>
               <span>4</span>
@@ -19,7 +19,7 @@ const NotFound = () => (
             Page Not Found
           </h4>
           <p className="text-gray-100 mt-1 mb-6 text-xl">
-            You wandered into uncharted territory. click on the button below to go home
+            You wandered into uncharted territory. click on the button below to go to home page
           </p>
           <Link
             to="/"
